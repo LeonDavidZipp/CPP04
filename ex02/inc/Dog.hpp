3d95fe21715b34d29112eec051b77627853d8ad3
Leon David Zipp <leondavidzipp@gmx.de>
2024-05-28 16:49:44 +0200

    added ex02 as copy of ex01, no new files added yet. removed other exercises not belonging on this branch

---
+++ ex02/inc/Dog.hpp
@@ -0,0 +1,35 @@
+#ifndef DOG_HPP
+# define DOG_HPP
+
+# include "Animal.hpp"
+# include "Brain.hpp"
+# include <iostream>
+# include <string>
+# include <array>
+
+class Dog : public Animal {
+    public:
+        Dog();
+        Dog(const std::string& type, Brain* brain);
+        Dog(const Dog& other);
+        ~Dog();
+
+        Dog& operator=(const Dog& other);
+
+        std::string getType() const;
+        Brain* getBrain() const;
+
+        void setType(std::string type);
+
+        void makeSound() const;
+
+        void compareTo(const Dog& other) const;
+
+    protected:
+        std::string _type;
+
+    private:
+        Brain* _brain;
+};
+
+#endif
