0cb698a917dd4e48c6e576de433f812e1d2814c9
Leon David Zipp <leondavidzipp@gmx.de>
2024-05-28 21:28:28 +0200

    added Ice methods

---
+++ ex03/src/Ice.cpp
@@ -0,0 +1,33 @@
+#include "../inc/Ice.hpp"
+
+Ice::Ice() : _type("default") {
+    std::cout << "Ice of type\t" << this->_type << " created" << std::endl;
+}
+
+Ice::Ice(const std::string& type) : _type(type) {
+    std::cout << "Ice of type\t" << type << " created" << std::endl;
+}
+
+Ice::Ice(const Ice& other) : _type(other._type) {
+    std::cout << "Ice created from\t" << other._type << std::endl;
+}
+
+Ice::~Ice() {
+    std::cout << "Ice of type\t" << this->_type << " destroyed." << std::endl;
+}
+
+Ice& Ice::operator=(const Ice& other) {
+    if (this != &other) {
+        this->_type = other._type;
+    }
+    std::cout << "Ice created from\t" << other._type << std::endl;
+    return *this;
+}
+
+std::string Ice::getType() const {
+    return this->_type;
+}
+
+void Ice::use(ICharacter& target) {
+    std::cout << "* shoots an ice bolt at\t" << target->getName() << " *" << std::endl;
+}
