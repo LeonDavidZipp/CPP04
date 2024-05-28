c52b02c8cec7bd03309756e23f18264b50d98607
Leon David Zipp <leondavidzipp@gmx.de>
2024-05-28 14:25:27 +0200

    adjusted cats methods to create or delete brain wherever necessary

--- ex01/src/Cat.cpp
+++ ex01/src/Cat.cpp
@@ -2,21 +2,25 @@
 
 Cat::Cat() : Animal() {
     this->_type = "Default Cat";
+    this->_brain = new Brain();
     std::cout << "Cat of type\t" << this->_type << " created" << std::endl;
 }
 
 Cat::Cat(const std::string& type) : Animal(type) {
     this->_type = type;
+    this->_brain = new Brain();
     std::cout << "Cat of type\t" << this->_type << " created" << std::endl;
 }
 
 Cat::Cat(const Cat& other) : Animal(other) {
     std::string otherType = other.getType();
     this->_type = otherType;
+    this->_brain = new Brain(*other._brain);
     std::cout << "Cat of type\t" << this->_type << " created from\t" << otherType << std::endl;
 }
 
 Cat::~Cat() {
+    delete this->_brain;
     std::cout << "Cat of type\t" << this->_type << " destroyed." << std::endl;
 }
 
@@ -24,6 +28,7 @@ Cat& Cat::operator=(const Cat& other) {
     std::string otherType = other.getType();
     if (this != &other) {
         this->_type = otherType;
+        this->_brain = new Brain(*other._brain);
     }
     std::cout << "Cat of type\t" << this->_type << " created from\t" << otherType << std::endl;
     return *this;
