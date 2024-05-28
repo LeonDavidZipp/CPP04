6b4e724d302b4a8c2110874398e9f35c40a079d1
Leon David Zipp <leondavidzipp@gmx.de>
2024-05-28 15:24:48 +0200

    fixed syntax erorrs

--- ex01/inc/Brain.hpp
+++ ex01/inc/Brain.hpp
@@ -8,13 +8,13 @@
 class Brain {
     public:
         Brain();
-        Brain(std::array<std::string, 100> ideas)
+        Brain(std::array<std::string, 100> ideas);
         Brain(const Brain &src);
         virtual ~Brain();
 
         Brain& operator=(const Brain &src);
 
-        virtual std:array<std::string, 100> getIdeas() const;
+        virtual std::array<std::string, 100> getIdeas() const;
         virtual std::string getIdea(int index) const;
         virtual void setIdeas(std::array<std::string, 100> ideas);
         virtual void setIdea(int index, std::string idea);
